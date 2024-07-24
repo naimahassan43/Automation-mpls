e2d58f5a5515c5f76ae31871dfdc7f87d6a34682
Naima Hassan <naima.hassan434@gmail.com>
2024-07-24 21:59:52 +0100

    Closing Connection

--- connect.py
+++ connect.py
@@ -1,3 +1,10 @@
 from netmiko import Netmiko
 
 connection = Netmiko(host = '192.168.150.111', port = '22' , username = 'admin', password = 'cisco', device_type='cisco_ios')
+
+output = connection.send_command('sh ip int br')
+print(output)
+
+
+print('Closing connection')
+connection.disconnect()
