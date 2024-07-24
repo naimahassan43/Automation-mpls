825c163e72ea5f17f5a361fe2cf48dfb47d8f542
Naima Hassan <naima.hassan434@gmail.com>
2024-07-24 22:22:44 +0100

    Connectionhandler & device dictionary

--- connect.py
+++ connect.py
@@ -1,7 +1,18 @@
-from netmiko import Netmiko
+#from netmiko import Netmiko
+#connection = Netmiko(host = '192.168.150.111', port = '22' , username = 'admin', password = 'cisco', device_type='cisco_ios')
 
-connection = Netmiko(host = '192.168.150.111', port = '22' , username = 'admin', password = 'cisco', device_type='cisco_ios')
+from netmiko import ConnectHandler
 
+cisco_device = {
+    'device_type':'cisco_ios',
+    'host' : '192.168.150.111', 
+    'username' : 'admin', 
+    'password' : 'cisco',
+    'port': '22',
+    'secret': 'cisco'
+}
+
+connection = ConnectHandler(**cisco_device)  
 output = connection.send_command('sh ip int br')
 print(output)
 
